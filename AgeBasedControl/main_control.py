7d9378fb262102bed607adef950290cc5fc5ba91
RyanJ119 <61523480+RyanJ119@users.noreply.github.com>
2024-03-21 17:49:05 -0400

    Add files via upload
    Update Model with newest code

---
+++ AgeBasedControl/main_control.py
@@ -0,0 +1,117 @@
+import numpy as np
+from utils import transform_to_have_essential_workers, read_data_from_csv, make_result_directory_for_simulation, Problem
+from plotting import generate_all_plots
+from plotting import print_heat_map
+from simulator import simulate
+#from controller import solve_control_problem
+#from ControllerCanibalizedW import solve_control_problem
+from controllerV2 import solve_control_problem
+import csv
+
+import time
+
+def _last_positive_component_in_group(S, idx):
+    indices = [i for i, s in enumerate(S[:, idx]) if s > 0.0]
+    if len(indices) > 0:
+        return max(indices) - 1
+    return len(S[:, idx])
+
+
+
+
+def main():
+    state_id = "NJ"
+    data = read_data_from_csv("../data/covid19_data_v1.csv")
+    state_data = data[state_id]
+    death_rates = np.array(
+        [
+            [
+                0.00016,
+                0.00016,
+                0.00006,
+                0.00006,
+                0.00007,
+                0.00007,
+                0.00229,
+                0.00229,
+                0.01915,
+                0.01915,
+                0.13527,
+            ]
+        ]
+    )
+    cost_lockdown = 70
+    num_age_groups = len(death_rates[0])
+    time_horizon = 180
+    #matrices = [(0.25, "../data/InteractionMatrix_beta_0_25.csv"), (0.5, "../data/InteractionMatrix_beta_0_5.csv"), (0.75,  "../data/InteractionMatrix_beta_0_75.csv")]
+    matrices = [(0.75,  "../data/InteractionMatrix_beta_0_75.csv")]
+    #R0s = [ 1.0, 1.1, 1.2]
+    R0s = [1.7]
+   # percentages_essential = [0.24, 0.34, 0.44]
+    percentages_essential = [0]
+    ordering = np.flip(list(range(num_age_groups)))
+    max_num_vaccines_per_day = np.sum(state_data["initial_S"]) * 0.6 / time_horizon
+    
+    with open('FL-Deaths.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Title", "baseline", "descent", "descent Es 5", "opt", "simOpt", "evenly spaced"])
+                    
+        for R0 in R0s:
+            for contact_matrix_pair in matrices:
+                contact_matrix = np.loadtxt(open(contact_matrix_pair[1], "rb"), delimiter=",")
+                for percentage_essential in percentages_essential:
+                    initial_S = transform_to_have_essential_workers(
+                        state_data["initial_S"], percentage_essential=percentage_essential
+                    )
+                    initial_E = transform_to_have_essential_workers(
+                        state_data["initial_E"], percentage_essential=percentage_essential
+                    )
+                    initial_I = transform_to_have_essential_workers(
+                        state_data["initial_I"], percentage_essential=percentage_essential
+                    )
+                    initial_R = transform_to_have_essential_workers(
+                        state_data["initial_R"], percentage_essential=percentage_essential
+                    )
+                    population = transform_to_have_essential_workers(
+                        state_data["population"], percentage_essential=percentage_essential
+                    )
+                    #initial_V = np.zeros((1, num_age_groups))
+                    
+                   # print(sum(sum(initial_I)))
+                    
+                    
+                    problem = Problem(
+                        time_horizon,
+                        R0,
+                        initial_S,
+                        initial_E*.6,
+                        initial_I*.6,
+                        initial_R,
+                        
+                        
+                        population,
+                        contact_matrix,
+                        death_rates,
+                        cost_lockdown,
+                        80
+                    )
+                   
+        
+    
+                    
+            
+               
+         
+                    S, E, I, R, w, cost = solve_control_problem(problem, max_num_vaccines_per_day)
+                    dir_path = make_result_directory_for_simulation(state_id, contact_matrix_pair[0], R0, percentage_essential, "opt_control_")
+                    print('deaths:')
+                    print(sum(sum(np.array(R)[-1,:]*death_rates)))
+                    generate_all_plots(
+                        dir_path, np.array(w), np.array(S), np.array(E), np.array(I), np.array(R), cost,  contact_matrix_pair[0], percentage_essential, cost_lockdown, problem, False
+                    )
+
+                    
+
+    
+if __name__ == "__main__":
+    main()
---
+++ AgeBasedControl/plotting.py
@@ -0,0 +1,186 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import seaborn as sb
+import pandas as pd
+import csv
+
+
+def generate_abstact_plot(
+        directory_path, name, cost, series, beta, percentage_essential, problem, show=False
+):
+    t = np.linspace(0, problem.time_horizon, problem.N + 1)
+    t=t[0:160]
+    plt.rcParams["figure.figsize"] = (8, 8)
+
+    plt.plot(t, series[:, 0], label="Age group 1")
+    plt.plot(t, series[:, 1], label="Age group 2")
+    plt.plot(t, series[:, 2], label="Age group 3")
+    #plt.plot(t, series[:, 3], label="Age group 3 Essential")
+    plt.plot(t, series[:, 4], label="Age group 4")
+    #plt.plot(t, series[:, 5], label="Age group 4 Essential")
+    plt.plot(t, series[:, 6], label="Age group 5")
+    #plt.plot(t, series[:, 7], label="Age group 5 Essential")
+    plt.plot(t, series[:, 8], label="Age group 6")
+    #plt.plot(t, series[:, 9], label="Age group 6 Essential")
+    plt.plot(t, series[:, 10], label="Age group 7", color='black')
+    plt.title(
+        f"{name}"
+    )
+    plt.legend()
+    plt.savefig(
+        os.path.join(directory_path, f"beta-{beta}-R0-{problem.R0}-PE-{percentage_essential}-{name}.png"),
+        dpi=300,
+        bbox_inches="tight",
+    )
+    if show:
+        plt.show()
+    plt.close()
+    
+def generate_abstact_plotU(
+        directory_path, name, cost, series, beta, percentage_essential, problem, show=False
+):
+    plt.rcParams.update({'font.size': 22})
+    t = np.linspace(0, problem.time_horizon, problem.N + 1)
+    t=t[0:160]
+    plt.rcParams["figure.figsize"] = (8, 8)
+    plt.plot(t, series[:, 0])
+    plt.ylim(top=1.5)  # adjust the top leaving bottom unchanged
+    plt.ylim(bottom=-.5)  # adjust the bottom leaving top unchanged
+    plt.title(
+        f"Lockdown of Elderly R0={problem.R0}"
+    )
+    plt.legend()
+    plt.savefig(
+        os.path.join(directory_path, f"Old-beta-{beta}-R0-{problem.R0}-PE-{percentage_essential}-{name}.png"),
+        dpi=300,
+        bbox_inches="tight",
+    )
+    if show:
+        plt.show()
+    plt.close()
+    
+    #t = np.linspace(0, problem.time_horizon-50, problem.N + 1)
+    plt.rcParams["figure.figsize"] = (8, 8)
+    plt.plot(t, series[:, 1])
+    plt.ylim(top=1.5)  # adjust the top leaving bottom unchanged
+    plt.ylim(bottom=-.50)  # adjust the bottom leaving top unchanged
+    plt.title(
+        f"Lockdown of School Age R0={problem.R0}"
+    )
+    plt.legend()
+    plt.savefig(
+        os.path.join(directory_path, f"School-beta-{beta}-R0-{problem.R0}-PE-{percentage_essential}-{name}.png"),
+        dpi=300,
+        bbox_inches="tight",
+    )
+    if show:
+        plt.show()
+    plt.close()
+    
+    
+    #t = np.linspace(0, problem.time_horizon, problem.N + 1)
+    plt.rcParams["figure.figsize"] = (8, 8)
+    plt.plot(t, series[:, 2])
+    plt.ylim(top=1.5)  # adjust the top leaving bottom unchanged
+    plt.ylim(bottom=-.50)  # adjust the bottom leaving top unchanged
+    plt.title(
+        f"Lockdown of Public R0={problem.R0} "
+    )
+    plt.legend()
+    plt.savefig(
+        os.path.join(directory_path, f"GenPop-beta-{beta}-R0-{problem.R0}-PE-{percentage_essential}-{name}.png"),
+        dpi=300,
+        bbox_inches="tight",
+    )
+    if show:
+        plt.show()
+    plt.close()
+
+
+    # t = np.linspace(0, problem.time_horizon, problem.N + 1)
+    # plt.rcParams["figure.figsize"] = (8, 8)
+    # plt.plot(t, series[:, 0]+(series[:, 2]+series[:, 1]), label="public plus schools")
+    # plt.ylim(top=1.5)  # adjust the top leaving bottom unchanged
+    # plt.ylim(bottom=-.50)  # adjust the bottom leaving top unchanged
+    # plt.title(
+    #     f"{name} R0{problem.R0} PE{percentage_essential} Cost: {cost}"
+    # )
+    # plt.legend()
+    # plt.savefig(
+    #     os.path.join(directory_path, f"public_Plus_School-beta-{beta}-R0-{problem.R0}-PE-{percentage_essential}-{name}.png"),
+    #     dpi=300,
+    #     bbox_inches="tight",
+    # )
+    # if show:
+    #     plt.show()
+    # plt.close()
+    
+def print_heat_map(
+         directory_path, w, S, E, I, R, beta, percentage_essential, problem, show=False
+):
+    cost_deaths = np.sum(R[-1, :] * problem.death_rates)
+    
+    days = np.linspace(0, problem.time_horizon, problem.N + 1)
+
+    vac_plan  = pd.DataFrame(np.array(w))
+
+    groups = ['0-4', '5-14', '15-19 non-essential', '15-19 essential', '20-39 non-essential', 
+
+              '20-39 essential', '40-59 non-essential','40-59 essential', '60-69 non-essential', 
+
+              '60-69 essential','70+']
+
+    plt.figure(figsize=(12, 12))
+
+    
+
+    sb.heatmap(np.transpose(np.array(vac_plan)), cmap='Blues', robust=True,
+
+           xticklabels=[day if day % 5 == 0 or day == max(days) else '' for day in days],
+
+           yticklabels=groups)
+
+    cax = plt.gcf().axes[-1]
+
+    cax.tick_params(labelsize=12)
+
+    plt.xticks(fontsize=12)
+
+    plt.yticks(fontsize=14)
+
+    plt.title(
+        f"Cost: {cost_deaths}"
+    )
+    plt.savefig(
+        os.path.join(directory_path, f"beta-{beta}-R0-{problem.R0}-PE-{percentage_essential}-Heat-Map-Vaccine-Policy.png"),
+        dpi=300,
+        bbox_inches="tight",
+    )
+    
+    if show:
+        plt.show()
+    plt.close()
+
+
+def generate_all_plots(directory_path, w, S, E, I, R,cost, beta, percentage_essential,cost_of_lockdown, problem, show=False):
+    #cost_deaths = np.sum(R[-1, :] * problem.death_rates)
+    #cost_lockdown= np.sum(((1-u))*cost_of_lockdown*S[-1, :])
+    #cost=1 #cost_deaths+cost_lockdown
+    generate_abstact_plotU(
+        directory_path, "Lockdown Policy", cost, w, beta, percentage_essential, problem, show
+    )
+    generate_abstact_plot(
+                directory_path, "Susceptible Per Day", cost, S, beta, percentage_essential, problem, show=False
+    )
+    generate_abstact_plot(
+                directory_path, "Exposed Per Day", cost, E, beta, percentage_essential, problem, show=False
+    )
+    generate_abstact_plot(
+                directory_path, "Infected Per Day", cost, I, beta, percentage_essential, problem, show=False
+    )
+    generate_abstact_plot(
+                directory_path, "Recovered Per Day", cost, R, beta, percentage_essential, problem, show=False
+    )
+
+
